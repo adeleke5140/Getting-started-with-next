fb3ccbf321181238dd5757ee2776a34eca6dd3fe
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-05-23 01:34:39 +0100

    add about page

---
+++ pages/about.js
@@ -0,0 +1,28 @@
+import Head from "next/head"
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>About Ninjas</title>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem ea culpa
+          explicabo ab modi! Deserunt, recusandae autem? Quas aliquid error
+          eveniet nostrum temporibus, aperiam ratione nesciunt corrupti
+          explicabo modi. Culpa?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quibusdam
+          nobis debitis quis sapiente at iusto quidem ea quia possimus facere
+          fugit ad temporibus quasi inventore, expedita fuga corrupti
+          architecto!
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default About
